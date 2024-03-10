90000e97b2fee3861dc1408652e41ab9b01ce963
MASINA Elliot <elliot.masina@epitech.eu>
2024-03-10 23:12:22 +0100

    [FEAT]: port cmd

--- src/commands/handlers/port.c
+++ src/commands/handlers/port.c
@@ -6,22 +6,41 @@
 */
 
 #include <stdio.h>
+#include <arpa/inet.h>
 #include "commands/cmd_tools.h"
 #include "commands/messages.h"
 
-static bool count_args(const char *args, const int fd)
+static int set_client_address(
+    struct data_s *client_data,
+    const int port_array[],
+    const int fd)
+{
+    char ip[MAX_PATH];
+
+    sprintf(ip, "%d.%d.%d.%d", port_array[0], port_array[1],
+        port_array[2], port_array[3]);
+    if (inet_aton(ip, &client_data->sock.sin_addr) == 0) {
+        write_message(fd, LOCAL_ERROR_451);
+        return ERROR;
+    }
+    client_data->sock.sin_family = AF_INET;
+    client_data->sock.sin_port = htons((port_array[4] * 256) + port_array[5]);
+    return SUCCESS;
+}
+
+static int count_args(const char *args, const int fd)
 {
     int nb_args = 0;
 
-    for (register int i = 0; args[i] != 0; i++) {
-        if (args[i] == ',')
+    for (register int index = 0; args[index] != 0; index++) {
+        if (args[index] == ',')
             nb_args++;
     }
     if (nb_args != 5) {
         write_message(fd, SYNTAX_ERROR_501);
-        return false;
+        return ERROR;
     }
-    return true;
+    return SUCCESS;
 }
 
 static bool check_args_port(const char *args, int port_array[], int fd)
@@ -34,9 +53,9 @@ static bool check_args_port(const char *args, int port_array[], int fd)
         write_message(fd, SYNTAX_ERROR_501);
         return false;
     }
-    for (register int i = 0; i < 6; i++) {
-        if (port_array[i] == ERROR
-            || port_array[i] < 0 || port_array[i] > 255) {
+    for (register int index = 0; index < 6; index++) {
+        if (port_array[index] == ERROR
+            || port_array[index] < 0 || port_array[index] > 255) {
             write_message(fd, SYNTAX_ERROR_501);
             return false;
         }
@@ -56,7 +75,8 @@ void cmd_port(
     (void)client;
     if (!is_logged(client_data, fd)
         || is_args_empty(args, fd)
-        || !check_args_port(args, port_array, fd))
+        || !check_args_port(args, port_array, fd)
+        || !set_client_address(client_data, port_array, fd))
         return;
     client_data->mode = ACTIVE;
     write_message(fd, PORT_200);
